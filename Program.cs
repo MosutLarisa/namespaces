c9ff9aa22705ba5f568ffdff328cad7858532373
MosutLarisa <larisa.mosut@emanuel.ro>
2023-10-23 09:31:12 +0300

    namespaces

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using namespaces.TwoD;
+using namespaces.ThreeD;
+using System.Drawing;
+
+namespace namespaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
